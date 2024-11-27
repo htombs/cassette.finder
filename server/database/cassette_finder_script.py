430d6d6c7db8f976cbbeee17571be62e0f921e3f
Harry Tombs <htombs91@gmail.com>
2024-11-27 14:29:46 +0000

    moved all cassettes data over to eight_speed.py renamed eight_speed.py to cassettes.py all functions from api.py and script.py now running from cassettes.py

--- server/database/cassette_finder_script.py
+++ server/database/cassette_finder_script.py
@@ -1,19 +1,4 @@
-import sqlite3
+from server.database.tables.cassettes import createCassetteTable, insertCassetteData
 
-from tables.eight_speed import insertEightSpeedData, createEightSpeedTable
-from tables.nine_speed import insertNineSpeedData, createNineSpeedTable
-from tables.ten_speed import insertTenSpeedData, createTenSpeedTable
-from tables.eleven_speed import insertElevenSpeedData, createElevenSpeedTable
-from tables import distributor_table
-
-createEightSpeedTable()
-insertEightSpeedData()
-
-createNineSpeedTable()
-insertNineSpeedData()
-
-createTenSpeedTable()
-insertTenSpeedData()
-
-createElevenSpeedTable()
-insertElevenSpeedData()
+createCassetteTable()
+insertCassetteData()
