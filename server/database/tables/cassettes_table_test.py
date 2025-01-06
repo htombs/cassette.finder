016aefe223a14c9137f37c19a7ea45512ec163fd
Harry Tombs <h.tombs91@gmail.com>
2025-01-06 15:37:25 +0000

    created seperate branch for cassettes_table_test.py

--- server/database/tables/cassettes_table_test.py
+++ server/database/tables/cassettes_table_test.py
@@ -1,10 +1,10 @@
 from unittest.mock import MagicMock,Mock
 import unittest
 import sqlite3
-from database.tables.eleven_speed import createElevenSpeedTable
+from database.tables.cassettes_table import createCassettesTable
 
-class TestElevenSpeed(unittest.TestCase):
-    def test_create_eleven_speed_table(self):
+class TestCassettesTable(unittest.TestCase):
+    def test_createCassettesTable(self):
         self.assertEqual(1, 1, "It's wrong")
 
 if __name__ == '__main__':
